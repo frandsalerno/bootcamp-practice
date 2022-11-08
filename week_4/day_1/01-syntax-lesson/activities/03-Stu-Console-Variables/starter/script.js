2ecb3f56190180f70c5e18af703e9464424e3207
Fran Salerno <salerno.fran@gmail.com>
2022-11-08 19:30:27 +0000

    Uploading first activity

--- week_4/day_1/01-syntax-lesson/activities/03-Stu-Console-Variables/starter/script.js
+++ week_4/day_1/01-syntax-lesson/activities/03-Stu-Console-Variables/starter/script.js
@@ -1,1 +1,10 @@
 // Write Your JavaScript Code Here
+var personName = "Fran Salerno";
+var pets = "2";
+var funFact = "I'm on a 2018 Football World Cup ad for Budweiser";
+
+console.log('My name is ' + personName);
+console.log('I have ' + pets +' pets');
+console.log('Fun fact: ' + funFact);
+
+console.log('I\'m ' + personName + ' and I have ' + pets + ' dogs');
