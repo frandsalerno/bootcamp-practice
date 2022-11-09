03238a85f7a4d63bb0f9c0a7c4d78a22919588ec
Fran Salerno <salerno.fran@gmail.com>
2022-11-09 20:26:35 +0000

    Uploading arrays practice

--- week_4/day_1/01-syntax-lesson/activities/09-Stu-Conditional-Statements/starter/script.js
+++ week_4/day_1/01-syntax-lesson/activities/09-Stu-Conditional-Statements/starter/script.js
@@ -2,6 +2,7 @@
 var x = 51;
 var expression1 = (x < 25);
 var expression2 = (x > 50);
+var output = expression1 && expression2;
 
 // Write Your JavaScript Code Here
 if (expression1 && expression2) {
@@ -13,3 +14,17 @@ if (expression1 && expression2) {
 } else{
     console.log('False ❌ False ❌');
 }
+
+// switch (output){
+//     case (true): 
+//     console.log ('True ✅ True ✅');
+//     break;
+//     case (false): 
+//     console.log('True ✅ False ❌');
+//     break;
+//     case (expression2): 
+//     console.log('False ❌ True ✅');
+//     break;
+//     default:
+//     console.log('False ❌ False ❌');  
+// }
--- week_4/day_2/02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
+++ week_4/day_2/02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
@@ -1,1 +1,15 @@
 // WRITE YOUR CODE HERE
+
+var students = ['Fran', 'Flor', 'Denu', 'Valentino']; 
+
+console.log(students.length);
+
+students.forEach(name => console.log('Welcome to the class ' + name));
+
+students[0] = 'Daniel';
+
+if (students[0] != 'Fran'){
+    console.log(students[0] + ' is in class');
+}
+
+console.log(students[students.length - 1]);
