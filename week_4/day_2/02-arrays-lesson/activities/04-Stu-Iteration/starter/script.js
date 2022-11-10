58fb106048b81e2e9bdbc78769153b9e1dd3ab85
Fran Salerno <salerno.fran@gmail.com>
2022-11-10 20:50:56 +0000

    Uploading the practice and the game

--- week_4/day_2/02-arrays-lesson/activities/04-Stu-Iteration/starter/script.js
+++ week_4/day_2/02-arrays-lesson/activities/04-Stu-Iteration/starter/script.js
@@ -3,12 +3,12 @@ var students = ['Fran', 'Flor', 'Valentino', 'Jaime', 'Jane'];
 
 console.log(students.length);
 
-// for (var index = 0; index < students.length ; index++){
-//     console.log('Great to see you, ' + students[index]);
-// }
+for (var i = 0; i < students.length ; i++){
+    console.log('Great to see you, ' + students[i]);
+}
 
-var index = 0;
-while (index < students.length){
-    console.log('Great to see you, ' + students[index]);
-    index++;
+var i = 0;
+while (i < students.length){
+    console.log('Great to see you, ' + students[i]);
+    i++;
 }
