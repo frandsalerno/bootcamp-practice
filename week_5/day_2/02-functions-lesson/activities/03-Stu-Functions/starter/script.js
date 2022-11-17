94d15c2e08501e815fcc8a23bd94badea70711c9
Fran Salerno <salerno.fran@gmail.com>
2022-11-17 20:42:09 +0000

    Uploading the functions practice

--- week_5/day_2/02-functions-lesson/activities/03-Stu-Functions/starter/script.js
+++ week_5/day_2/02-functions-lesson/activities/03-Stu-Functions/starter/script.js
@@ -1,1 +1,23 @@
 // Write Your Code Below
+function add(i, j){
+    console.log(i+j);
+}
+
+add(1,2);
+
+function sub(i, j){
+    console.log(i+j);
+}
+
+var sub = function(first, second) {
+    console.log(first-second);
+  };
+
+sub(20,10);
+
+var side = 20;
+
+function square(a){
+    console.log(a*a);
+}
+square(side);
