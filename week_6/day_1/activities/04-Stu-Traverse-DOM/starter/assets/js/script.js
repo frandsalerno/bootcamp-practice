6b8072b5c8f012212d51070ba89393ef00b0a7d3
Fran Salerno <salerno.fran@gmail.com>
2022-11-29 19:28:19 +0000

    Changes on password generator

--- week_6/day_1/activities/04-Stu-Traverse-DOM/starter/assets/js/script.js
+++ week_6/day_1/activities/04-Stu-Traverse-DOM/starter/assets/js/script.js
@@ -1,7 +1,16 @@
-// Access element using id
-var articlesDiv = document.getElementById('articles');
-var headerDiv = document.getElementById('header');
+// // Access element using id
+// var articlesDiv = document.getElementById('articles');
+// var headerDiv = document.getElementById('header');
 
-// Change style by accessing style object's properties
-articlesDiv.children[2].style.fontSize = '50px';
-headerDiv.style.color = 'white';
+// // Change style by accessing style object's properties
+// articlesDiv.children[2].style.fontSize = '50px';
+// headerDiv.style.color = 'white';
+
+
+var header = document.getElementById('header');
+// console.log(header);
+header.children[0].style.color =  '#FFF';
+
+var article = document.getElementById('articles');
+console.log(article.children[0].children[1]);
+article.children[0].children[1].style.fontSize = '50px';
