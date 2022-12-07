0df767d7e3890b6ab482f3cc2a08e5a975c544ab
Fran Salerno <salerno.fran@gmail.com>
2022-12-07 18:55:25 +0000

    Uploading practice from day 2

--- week_7/day1/01-jquery-lesson/activities/02-Stu-jQuery-Elements/starter/assets/js/script.js
+++ week_7/day1/01-jquery-lesson/activities/02-Stu-jQuery-Elements/starter/assets/js/script.js
@@ -2,11 +2,10 @@
 // Hint: Visit the documentation https://api.jquery.com/
 
 var root = $('#root');
-console.log(root);
 
 // TODO: Create a `<p>` element that will store an author's name and assign that created element to a new variable
 
-var p = $( "p" ).add( "<p>Author's name</p>" );
+var p = $('<p>');
 
 // TODO: Add the following text to the newly created `<p>` element: "~ Carol Dweck"
 
@@ -16,11 +15,10 @@ p.text('~ Carol Dweck');
 // TODO: Add the class `plain` to the author element
 
 p.addClass('plain');
-console.log(p);
 
 // TODO: Create a new `<h1>` element that will store an author's quote and assign it to new variable
 
-var h1 = $('h1').add('<h1></h1>')
+var h1 = $('<h1>')
 
 // TODO: Add the following quote text to the quote element, "Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning."
 
--- week_7/day1/scripts.js
+++ week_7/day1/scripts.js
@@ -6,8 +6,22 @@ $('p').css('color','blue');
 
 // var p2 = document.querySelector('p:nth-child(3)');
 
-var p2 = $('p:nth-child(3)');
+var p2 = $('.second');
 
-p2.animate({
-    left: '+=50px'
-},4000)
+// p2.animate({
+//     left: '+=50px'
+// },4000)
+
+// p2.click(function(){
+//     console.log('click');
+// })
+
+var paras = $('p');
+
+paras.click(function(){
+    console.log($(this).text());
+})
+
+paras.each(function(){
+    console.log($(this).text());
+})
