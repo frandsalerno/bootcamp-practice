44e6162d6a234b0938e52bea15ab6ba30e245584
Fran Salerno <salerno.fran@gmail.com>
2022-12-14 14:50:54 +0000

    Updating the moviesapp

---
+++ week_8/day_1/movie_app/movies.js
@@ -0,0 +1,33 @@
+var movieData = [
+    {
+        title: 'Jurassic Park',
+        description: 'a movie about dinosaurs',
+        imdb_link: 'https://www.imdb.com/chart/top/',
+        movie_image: 'https://api.lorem.space/image/game?w=150&h=220'
+    },
+    {
+        title: 'Matrix',
+        description: 'a movie about dinosaurs',
+        imdb_link: 'https://www.imdb.com/chart/top/',
+        movie_image: 'https://api.lorem.space/image/game?w=150&h=220'
+    },
+    {
+        title: 'Amelie',
+        description: 'a movie about dinosaurs',
+        imdb_link: 'https://www.imdb.com/chart/top/',
+        movie_image: 'https://api.lorem.space/image/game?w=150&h=220'
+    },
+    {
+        title: 'Godfather',
+        description: 'a movie about dinosaurs',
+        imdb_link: 'https://www.imdb.com/chart/top/',
+        movie_image: 'https://api.lorem.space/image/game?w=150&h=220'
+    },
+    {
+        title: 'Back to the future',
+        description: 'a movie about dinosaurs',
+        imdb_link: 'https://www.imdb.com/chart/top/',
+        movie_image: 'https://api.lorem.space/image/game?w=150&h=220'
+    }
+
+];
