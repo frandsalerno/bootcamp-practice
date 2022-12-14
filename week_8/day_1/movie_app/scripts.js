b671463afb21e8c7bdc263fba966536f02d29929
Fran Salerno <salerno.fran@gmail.com>
2022-12-14 12:29:06 +0000

    uploading all the movie app code

---
+++ week_8/day_1/movie_app/scripts.js
@@ -0,0 +1,6 @@
+/* <div class="movie-card">
+<h3>Movie Title</h3>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, modi corporis. Quaerat, fugiat tempore. At praesentium culpa optio ipsam quidem corporis, modi necessitatibus perspiciatis omnis reprehenderit voluptatem, veritatis iste dolorem.</p>
+<a href="#">View More Info Here</a>
+</div> */
+
