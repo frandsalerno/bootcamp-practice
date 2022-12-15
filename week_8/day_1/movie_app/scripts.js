ff337c6fc64ef387ba5a8df646ec17a9fa431a14
Fran Salerno <salerno.fran@gmail.com>
2022-12-15 15:24:32 +0000

    Changes applied with the api implementation done

--- week_8/day_1/movie_app/scripts.js
+++ week_8/day_1/movie_app/scripts.js
@@ -1,18 +1,50 @@
-/* <div class="movie-card">
-<h3>Movie Title</h3>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, modi corporis. Quaerat, fugiat tempore. At praesentium culpa optio ipsam quidem corporis, modi necessitatibus perspiciatis omnis reprehenderit voluptatem, veritatis iste dolorem.</p>
-<a href="#">View More Info Here</a>
-</div> */
+//API key http://www.omdbapi.com/?i=tt3896198&apikey=4bc5592f
+
+// http://www.omdbapi.com/?apikey=4bc5592f
+
+
+// giphy API LHe9yDHuczqtPqiwKb2W2h9egFk5cYMh
 
 var searchInput = document.querySelector('.search');
-
 var cardWrapper = document.querySelector('main');
+
+function noMatch(){
+    cardWrapper.innerHTML = '<p class="text-center no-search">No results found</p>';
+}
+
+function displayMatches(matches){
+    cardWrapper.innerHTML = '';
+
+    if (!matches.length){
+        noMatch();
+    }
+
+    for (var matchObject of matches){
+        cardWrapper.insertAdjacentHTML("beforeend",`
+        <div class="movie-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${matchObject.movie_image});">
+        <h3>${matchObject.title}</h3>
+        <p>${matchObject.description}</p>
+        <a href="${matchObject.imdb_link}" target="_blank">View More Info Here</a>
+        </div>`)
+    }
+}
+
 
 function fetchMovies(event){
     var keyCode = event.keyCode;
-    var searchText = searchInput.value;
+    var searchText = searchInput.value.toLowerCase().trim();
     if (keyCode === 13 && searchText){
-        console.log('it works');
+        var matches = []
+        
+        fetch(`https://www.omdbapi.com/?apikey=4bc5592f&t=${searchText}`).then(function(responseObj){
+            var dataPromise = responseObj.json();
+            dataPromise.then(function(data){
+                console.log(data);
+            })
+        });
+
+        displayMatches(matches);
+
     }
 }
 
